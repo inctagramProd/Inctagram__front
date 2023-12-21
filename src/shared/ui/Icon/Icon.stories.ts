1a76e603063f4f6f3578f3e1df195dba9b562609
stasikrus <stas-boss@mail.ru>
2023-12-21 18:53:34 +0300

    refactor: Update Icon component

--- src/shared/ui/Icon/Icon.stories.ts
+++ src/shared/ui/Icon/Icon.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { Icon } from './Icon'
+import Icon from './Icon'
 
 const meta = {
   title: 'Components/Icons',
@@ -15,49 +15,8 @@ type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
   args: {
-    value:0,
+    iconName: 'Bell',
     width: 24,
-    height:24,
-    fillType: 'outline',
-    theme: 'dark',
+    height: 24,
   },
 }
-export const Light: Story = {
-  args: {
-    value:0,
-    width: 24,
-    height:24,
-    fillType: 'outline',
-    theme: 'light',
-  },
-}
-export const Dark: Story = {
-  args: {
-    value:0,
-    width: 24,
-    height:24,
-    fillType: 'outline',
-    theme: 'dark',
-  },
-}
-export const Outline: Story = {
-  args: {
-    value:0,
-    width: 24,
-    height:24,
-    fillType: 'outline',
-    theme: 'dark',
-  },
-}
-export const Fill: Story = {
-  args: {
-    value:0,
-    width: 24,
-    height:24,
-    fillType: 'fill',
-    theme: 'dark',
-  },
-}
-
-
-
--- src/shared/ui/Icon/Icon.tsx
+++ src/shared/ui/Icon/Icon.tsx
@@ -1,75 +1,43 @@
-import React, { ComponentPropsWithoutRef, useState } from 'react'
 import * as Icons from '../../assets/icons/icons'
-import { Iicon } from './../../assets/icons/icons';
 
-
-
-export const Icon= ({ width=24, height=24, fillType='outline',theme='dark',value}:Iicon) => {
-  let bg=theme==='dark'?'dark':'white'
-    return (
-      <div className='flex-col'>
-        <div className={`bg-${bg} w-[100%] flex `}>
-          <Icons.ArrowIosBack width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.ArrowIosForward width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.Bell width={width} height={height} fillType={fillType} theme={theme} value={value}/>
-          <Icons.Block width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.Close width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.Expand width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.Menu width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.Picture width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.PlusCircle width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.Search width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.Settings width={width} height={height} fillType={fillType} theme={theme}/>
-      </div>
-      <div className={`bg-${bg} w-[100%] flex `}>
-          <Icons.PlayCircle width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.PauseCircle width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.RadioBtnChk width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.ArrowIosUp width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.ArrowIosDown width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.Calendar width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.LogOut width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.Bookmark width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.TrendingUp width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.Person width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.PlusSquare width={width} height={height} fillType={fillType} theme={theme}/>
-      </div>
-      <div className={`bg-${bg} w-[100%] flex `}>
-          <Icons.HomeIcon width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.ArrowBack width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.ArrowForward width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.Palette width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.Layers width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.EyeOff width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.Eye width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.PaperPlane width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.MoreHorizontal width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.MessageCircle width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.PersonRemove width={width} height={height} fillType={fillType} theme={theme}/>
-      </div>
-      <div className={`bg-${bg} w-[100%] flex `}>
-          <Icons.PersonAdd width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.Mic width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.Copy width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.Pin width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.Heart width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.Email width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.Trash width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.Edit width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.Maximize width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.CreditCard width={width} height={height} fillType={fillType} theme={theme}/>
-      </div>
-      <div className={`bg-${bg} w-[100%] flex `}>
-          <Icons.GithubLogo width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.Recaptcha width={width} height={height}/>
-          <Icons.FaceBookLogo width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.GoogleLogo width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.RussiaFlag width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.EnglishFlag width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.PayPal width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.Stripe width={width} height={height} fillType={fillType} theme={theme}/>
-          <Icons.Paid width={width} height={height} fillType={fillType} theme={theme}/>
-      </div>
-      </div>
-    )   
+type IconProps = {
+  iconName: string
+  width?: number
+  height?: number
+  fillType?: 'fill' | 'outline'
+  theme?: 'light' | 'dark'
+  value?: number
 }
+
+type SvgIconComponent = React.FC<IconProps>
+
+type IconsType = {
+  [key: string]: SvgIconComponent | undefined
+}
+
+const Icon = ({
+  iconName,
+  width = 24,
+  height = 24,
+  fillType = 'outline',
+  theme = 'dark',
+  value,
+}: IconProps) => {
+  const IconComponent = (Icons as IconsType)[iconName]
+  if (!IconComponent) {
+    return null
+  }
+
+  return (
+    <IconComponent
+      width={width}
+      height={height}
+      fillType={fillType}
+      theme={theme}
+      value={value}
+      iconName={iconName}
+    />
+  )
+}
+
+export default Icon
