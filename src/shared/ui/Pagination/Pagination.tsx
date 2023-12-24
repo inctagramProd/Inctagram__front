80d9d367a9c4aee3b7ee584c2a6218676b935e9c
Timur Makeev <timmak09@gmail.com>
2023-12-24 23:08:57 +0300

    Pagination component: Added main logic

--- src/shared/ui/Pagination/Pagination.tsx
+++ src/shared/ui/Pagination/Pagination.tsx
@@ -9,6 +9,29 @@ interface IPaginationProps {
     onChange?: (selected: number) => void;
 }
 
+const numberToArray = (n) => {
+    return Array.from({length: n}, (_, i) => i + 1);
+}
+
+const transformToPagination = (arr, selected) => {
+    if (arr.length < 7) {
+        return arr;
+    }
+
+    let index = arr.indexOf(selected);
+    let result = [];
+
+    if (index <= 2) {
+        result = arr.slice(0, 5).concat(['...'], arr.slice(-1));
+    } else if (index >= arr.length - 3) {
+        result = arr.slice(0, 1).concat(['...'], arr.slice(-6));
+    } else {
+        result = arr.slice(0, 1).concat(['...'], arr.slice(index - 1, index + 2), ['...'], arr.slice(-1));
+    }
+
+    return result;
+}
+
 export const Pagination = ({
    total,
    defaultCurrent = 1,
@@ -19,6 +42,7 @@ export const Pagination = ({
     const [itemsPerPage, setItemsPerPage] = useState(10);
 
     const pagesCount = Math.ceil(total / itemsPerPage);
+    const pagesArray = transformToPagination(numberToArray(pagesCount), activePage);
 
     // Need fix
     const interFont = {
@@ -26,53 +50,62 @@ export const Pagination = ({
     };
 
     const handleSetActivePage = (page: React.SetStateAction<number>) => {
-        if(page === activePage) return;
+        if(page === activePage || !Number.isInteger(page)) return;
         setActivePage(page);
     }
 
     const PaginationPages = () => {
-        const elements = [];
-        for(let i = 0; i <= pagesCount + 1; i++) {
-            let title: number | string = i;
-            let onChangeFunc = () => {
-                handleSetActivePage(i)
-            }
+        const paginationElements: any = [];
 
-            if(i == 0) {
-                title = '<';
-                onChangeFunc = () => {
-                    const newPage = activePage < 2 ? 1 : activePage - 1;
-                    handleSetActivePage(newPage)
-                }
-            }
+        const handlePrevClick = () => {
+            const newPage = Math.max(activePage - 1, 1);
+            handleSetActivePage(newPage);
+        };
 
-            if(i > pagesCount) {
-                title = '>';
-                onChangeFunc = () => {
-                    const newPage = activePage < pagesCount ? activePage + 1 : pagesCount;
-                    handleSetActivePage(newPage)
-                }
-            }
+        const handleNextClick = () => {
+            const newPage = Math.min(activePage + 1, pagesCount);
+            handleSetActivePage(newPage);
+        };
 
-            elements.push(
+        pagesArray.map((page: any, i: any) => {
+            return paginationElements.push(
                 <li
-                    className={`${styles.pagination__item} ${activePage === i ? styles.pagination__items_active : ''}`}
-                    onClick={onChangeFunc}
+                    key={page}
+                    className={`${styles.pagination__item} ${activePage === page ? styles.pagination__items_active : ''}`}
+                    onClick={() => handleSetActivePage(page)}
                     tabIndex={0}
                 >
-                    {title}
+                    {page}
                 </li>
-            )
-        }
-        return <ul className={`${styles.pagination__items}`}>
-            {elements}
-        </ul>;
-    }
+            );
+        })
+
+        return (
+            <ul className={styles.pagination__items}>
+                <li
+                    className={`${styles.pagination__item} ${activePage === 1 ? styles.pagination__items_disabled : ''}`}
+                    onClick={handlePrevClick}
+                    tabIndex={0}
+                >
+                    {'<'}
+                </li>
+                {paginationElements}
+                <li
+                    className={`${styles.pagination__item} ${activePage === pagesCount ? styles.pagination__items_disabled : ''}`}
+                    onClick={handleNextClick}
+                    tabIndex={0}
+                >
+                    {'>'}
+                </li>
+            </ul>
+        );
+    };
 
     const PaginationItemsPerPage = () => {
         const itemsPerPageList = [10, 20, 30, 50, 100];
         const itemPerPageSelect = <Select
             defaultValue={parseInt(itemsPerPage)}
+            variant={"Pagination"}
             onChange={(selectedValue: object) => {
                 setItemsPerPage(selectedValue.value);
                 setActivePage(1);
