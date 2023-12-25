e640b822f9b9402baf39e2fed0d0c94ad856b59b
Pavel-Vladimirovich <nesoglasnyj.pavel@gmail.com>
2023-12-25 17:52:42 +0300

    UC-38 feat: add ReCaptcha component

--- src/shared/ui/Card/Card.stories.ts
+++ src/shared/ui/Card/Card.stories.ts
@@ -7,12 +7,6 @@ const meta: Meta<typeof Card> = {
     tags: ['autodocs'],
     parameters: {
         layout: 'centered',
-        backgrounds: {
-            values: [
-                { name: 'background', value: '#0D0D0D' },
-            ],
-        },
-
     },
     args: {
         children: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aut blanditiis, deleniti dolor eius eum molestias mollitia nesciunt officia pariatur quae repudiandae sed unde! Accusamus alias architecto doloremque dolores ducimus eaque eligendi eos fugit, id iste libero nam, nesciunt nostrum optio perspiciatis praesentium rem repellat, sint soluta ut vitae voluptatibus.'
@@ -23,7 +17,7 @@ export default meta
 
 type Story = StoryObj<typeof Card>
 
-export const CardComponent: Story = {
+export const Default: Story = {
 
 }
 
---
+++ src/shared/ui/ReCaptcha/ReCaptcha.tsx
@@ -0,0 +1,32 @@
+import ReCAPTCHA, {ReCAPTCHAProps} from 'react-google-recaptcha';
+import styles from './reCaptcha.module.css';
+
+type ReCaptchaPropsType = {
+    /** Text error message */
+    errorMessage?: string | null
+    // The function onChange to be called when the user successfully completes the normal or the compact captcha.
+    // It will also be called with null, when captcha expires.
+    onChange: (value: string | null) => void
+    theme?: 'dark' | 'light'
+} & Omit<ReCAPTCHAProps, 'sitekey'>;
+
+/**
+ * ReCAPTCHA UI component
+ */
+
+export const ReCaptcha = (props: ReCaptchaPropsType) => {
+
+    const {theme = 'dark', errorMessage, ...restProps} = props
+
+    return (
+        <div className={`${styles.wrapper} ${errorMessage && styles.errorBorder}`}>
+            <ReCAPTCHA sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!} {...restProps} theme={theme}
+                       hl={'en-GB'}/>
+            {errorMessage &&
+                <span className={styles.errorMessage}>{errorMessage}</span>}
+        </div>
+    );
+};
+
+// !!!! you should add a .env.local file with this key:
+// NEXT_PUBLIC_RECAPTCHA_SITE_KEY=6LepKzspAAAAACORjdRAQAuzGUTmkMzIPBb_5z7f
