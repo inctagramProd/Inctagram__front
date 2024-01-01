42c932a892fea8a7ebc3104170ac0b853a467bc9
TimurMakeev <timmak09@gmail.com>
2024-01-01 17:09:23 +0300

    UC-31 refactor: ts and styles improvements

--- src/shared/ui/Select/Select.tsx
+++ src/shared/ui/Select/Select.tsx
@@ -1,115 +1,139 @@
-import React, {useRef, useState, useEffect} from 'react';
-import styles from './select.module.css';
-import ArrowBottom from "@/src/shared/assets/icons/ArrowBottom";
+import React, { useRef, useState, useEffect } from 'react'
+import { Typography } from '@/src/shared/ui/Typography/Typography'
+import Icon from '@/src/shared/ui/Icon/Icon'
 
-interface ISelectProps {
-    title?: string;
-    disabled?: boolean;
-    defaultValue?: string | number;
-    variant?: 'Default' | 'Pagination';
-    options: {
-        title: string | number;
-        value: string | number;
-    }[];
-    onChange?: (selectedValue: object) => void;
+type SelectOptionType = {
+  title: string | number
+  value: string | number
+}
+
+type TypeProps = {
+  title?: string
+  disabled?: boolean
+  defaultValue?: string | number
+  variant?: 'Default' | 'Pagination'
+  options: SelectOptionType[]
+  onChange?: (selectedValue: SelectOptionType) => SelectOptionType
 }
 
 export const Select = ({
-    title,
-    options,
-    defaultValue,
-    variant = 'Default',
-    disabled = false,
-    onChange,
-    ...props
-}: ISelectProps) => {
-    const defaultOption = defaultValue
-        ? options.find(option => option.value === defaultValue)
-        : options[0];
+  title,
+  options,
+  defaultValue,
+  variant = 'Default',
+  disabled = false,
+  onChange,
+  ...props
+}: TypeProps) => {
+  const defaultOption: SelectOptionType | undefined = defaultValue
+    ? options.find((option: SelectOptionType) => option.value === defaultValue)
+    : options[0]
 
-    const [activeEl, setActiveEl] = useState({
-        title: defaultOption?.title || '',
-        value: defaultOption?.value || '',
-    });
+  const [activeOption, setActiveOption] = useState({
+    title: defaultOption?.title || '',
+    value: defaultOption?.value || '',
+  })
 
-    const [isVisibleDropdown, setIsVisibleDropdown] = useState(false);
+  const [isVisibleDropdown, setIsVisibleDropdown] = useState(false)
 
-    // Close dropdown if click outside
-    const wrapperRef: React.RefObject<HTMLInputElement> = useRef<HTMLInputElement>(null);
-    useEffect(() => {
-        document.addEventListener("click", handleClickOutside, false);
-        return (): void => {
-            document.removeEventListener("click", handleClickOutside, false);
-        };
-    }, []);
+  // Close dropdown if click outside
+  const wrapperRef: React.RefObject<HTMLInputElement> = useRef<HTMLInputElement>(null)
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside, false)
+    return (): void => {
+      document.removeEventListener('click', handleClickOutside, false)
+    }
+  }, [])
 
-    const handleClickOutside = (e: { target: any; }): void => {
-        if (wrapperRef.current && !wrapperRef.current.contains(e.target)) {
-            setIsVisibleDropdown(false);
-        }
-    };
+  const handleClickOutside = (e: { target: any }): void => {
+    if (wrapperRef.current && !wrapperRef.current.contains(e.target)) {
+      setIsVisibleDropdown(false)
+    }
+  }
 
-    const setDropdownActiveEl = (): void => {
-        if(isVisibleDropdown) {
-            setIsVisibleDropdown(false);
-        } else {
-            setIsVisibleDropdown(true);
-        }
+  const setDropdownActiveOption = (): void => {
+    if (isVisibleDropdown) {
+      return setIsVisibleDropdown(false)
+    }
+    return setIsVisibleDropdown(true)
+  }
+
+  const handleSetActiveOption = (e: React.SyntheticEvent<EventTarget>): void => {
+    const target = e.target as HTMLElement
+    const { title, value } = target.dataset
+
+    if (!title || !value) {
+      return
     }
 
-    const handleSetActiveEl = (e: { target: any; }): void => {
-        const { title, value } = e.target.dataset;
-        const activeElFields = { title, value };
-
-        setActiveEl(activeElFields);
-
-        if (onChange) {
-            onChange(activeElFields);
-        }
+    const activeOptionFields: SelectOptionType = { title, value }
+    if (activeOptionFields) {
+      setActiveOption(activeOptionFields)
     }
 
-    // Need fix
-    const interFont = {
-        fontFamily: 'Inter, sans-serif'
-    };
+    if (onChange) {
+      onChange(activeOptionFields)
+    }
+  }
 
-    return (
-        <div style={interFont} className={`${styles.select} ${disabled ? styles.disabled : ''} ${variant === 'Pagination' ? styles.select_pagination : ''} `}  {...props}>
-            <span className={`${styles.select__title}`}>{title}</span>
-            <div
-                ref={wrapperRef}
-                onClick={setDropdownActiveEl}
-                className={`${styles.select__active_el} ${isVisibleDropdown ? styles.select__active_el_selected : ''}`}
-                tabIndex={0}
-            >
-                <span className={`${styles.select__dropdown_title}`}>
-                    {activeEl.title}
-                </span>
-                <div
-                    className={`${isVisibleDropdown ? styles.select__dropdown_arrow_active : ''} ${styles.select__dropdown_icon}`}
-                >
-                    <ArrowBottom />
-                </div>
-            </div>
-
-            <ul className={` ${styles.select__dropdown} ${isVisibleDropdown ? styles.select__dropdown_visible : ''} `}>
-                {
-                    options.map((item) => (
-                        <li
-                            key={item.value}
-                            data-value={item.value}
-                            data-title={item.title}
-                            aria-selected={activeEl.value == item.value}
-                            role={'option'}
-                            onClick={handleSetActiveEl}
-                            className={`${styles.select__dropdown_item} ${activeEl.value == item.value ? styles.select__dropdown_item_selected : ''}`}
-                            tabIndex={-1}
-                        >
-                            {item.title}
-                        </li>
-                    ))
-                }
-            </ul>
+  return (
+    <div
+      className={`max-w-sm relative disabled:opacity-10 ${disabled ? 'cursor-not-allowed' : ''}`}
+      {...props}
+    >
+      <span className={`text-light-900 block text-sm`}>
+        <Typography children={title} variant={'regular_14'} />
+      </span>
+      <div
+        ref={wrapperRef}
+        onClick={setDropdownActiveOption}
+        className={`border-light-900 flex items-center justify-between gap-2 select-none w-full cursor-pointer text-light-100 border px-2 py-1.5 rounded-sm hover:text-light-900 ${
+          disabled ? 'pointer-events-none border-dark-100' : ''
+        } ${isVisibleDropdown ? 'border-light-100 bg-dark-500' : ''} ${
+          variant === 'Pagination'
+            ? 'px-2 py-0.5 bg-dark-500 border-dark-300 text-light-100 text-sm'
+            : ''
+        }`}
+        tabIndex={0}
+      >
+        <span
+          className={`flex flex-row items-center gap-2 ${
+            disabled ? 'text-dark-100 text-base' : ''
+          }`}
+        >
+          <Typography children={activeOption.title} variant={'regular_16'} />
+        </span>
+        <div
+          className={`w-5 h-5 flex justify-center items-center transform-gpu transition-transform ${
+            isVisibleDropdown ? 'rotate-180' : ''
+          }`}
+        >
+          <Icon iconName={'ArrowIosDown'} width={20} height={20} />
         </div>
-    );
+      </div>
+
+      <ul
+        className={`transition-all origin-top ease-in-out max-h-72 overflow-y-auto absolute w-full border bg-dark-500 rounded-b-sm -mt-px opacity-0 z-[-1] transform-gpu scale-y-0 ${
+          isVisibleDropdown ? 'opacity-100 z-10 transform-gpu scale-y-100' : ''
+        } ${variant === 'Pagination' ? 'bg-dark-500 border-dark-300' : ''}`}
+      >
+        {options.map((item: SelectOptionType) => (
+          <li
+            key={item.value}
+            data-value={item.value}
+            data-title={item.title}
+            aria-selected={activeOption.value == item.value}
+            role={'option'}
+            onClick={handleSetActiveOption}
+            className={`py-2 px-4 flex flex-row items-center gap-2 cursor-pointer select-none text-light-100 hover:bg-dark-300 hover:text-primary-500 text-base ${
+              activeOption.value == item.value ? 'bg-dark-300 text-primary-500' : ''
+            } ${variant === 'Pagination' ? 'px-2 py-0.5 text-light-100 text-sm' : ''}`}
+            tabIndex={-1}
+          >
+            <Typography children={item.title} variant={'regular_14'} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
