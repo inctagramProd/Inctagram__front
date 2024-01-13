7562a67d739e7b12a90ba7744a1d38659e522d0f
Alexander Ivantsov <alexander.ivantsov.hw@gmail.com>
2024-01-13 18:01:36 +0300

    fix: fix the iconName bug in button

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -29,7 +29,7 @@ type Props = {
   //Icon properties
   height?: number
   width?: number
-  iconName?: string
+  iconName: string
   iconStyle?: string
   fillType?: 'fill' | 'outline'
   handleFocus?: () => void
