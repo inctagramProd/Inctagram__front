6fcf54deff0989d1d0d0d905943a6eafdc5530b6
Alexander Ivantsov <alexander.ivantsov.hw@gmail.com>
2024-01-13 18:47:51 +0300

    fix: fix sidebar stories

--- src/widgets/SideBar/Sidebar.stories.ts
+++ src/widgets/SideBar/Sidebar.stories.ts
@@ -13,9 +13,7 @@ const meta = {
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+  argTypes: {},
 } satisfies Meta<typeof Sidebar>
 
 export default meta
@@ -24,11 +22,6 @@ type Story = StoryObj<typeof meta>
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Dark: Story = {
   args: {
-    theme:'dark' 
-  },
-}
-export const Light: Story = {
-  args: {
-    theme:'light' 
+    theme: 'dark',
   },
 }
