334ab3a51afc7478dbaf1f4376feb8d474c2a243
TimurMakeev <timmak09@gmail.com>
2024-01-15 00:00:38 +0300

    UC-64 fix: fixed main container paddings

--- src/widgets/Layout/BaseLayout/BaseLayout.tsx
+++ src/widgets/Layout/BaseLayout/BaseLayout.tsx
@@ -1,13 +1,12 @@
 import { NextPage } from 'next'
 import { PropsWithChildren } from 'react'
 import { Header } from '@/src/widgets/Header/Header'
-import { Sidebar } from '@/src/widgets/Sidebar/Sidebar'
 
 export const BaseLayout: NextPage<PropsWithChildren> = ({ children }) => {
   return (
     <div>
       <Header className={'sticky top-0 w-full z-[991] max-sm:fixed'} />
-      <div className={'max-w-screen-xl m-auto flex flex-row max-sm:flex-col'}>
+      <div className={'max-w-screen-xl m-auto flex flex-row px-4 max-sm:flex-col'}>
         <div className={'sm:flex-1 max-sm:mt-[60px] overflow-hidden'}>{children}</div>
       </div>
     </div>
