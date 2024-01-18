6321c4241fbf9dfde5f1d7c4747947ba19c713cc
krlkonstantine <krlkonstantine@gmail.com>
2024-01-18 12:07:57 +0300

    UC-39 feat: add error style

--- src/shared/ui/DatePicker/DatePicker.tsx
+++ src/shared/ui/DatePicker/DatePicker.tsx
@@ -1,65 +1,70 @@
-import React, {useState} from 'react';
-import DatePicker from 'react-datepicker';
-import {format} from 'date-fns'; // Import the format function
-import 'react-datepicker/dist/react-datepicker.css';
-import s from './DatePicker.module.css';
+import React, { useState } from 'react'
+import DatePicker from 'react-datepicker'
+import { format } from 'date-fns' // Import the format function
+import 'react-datepicker/dist/react-datepicker.css'
+import s from './DatePicker.module.css'
 import './style.css'
-type PropsType = {
-    isRange?: boolean;
-    onChange?: (value: string) => void;
-};
+import * as Icons from '../../assets/icons/icons'
 
-type CustomInputProps = {
-    value: string;
-    onClick: () => void;
-};
+type Props = {
+  isRange?: boolean
+  onChange?: (value: string) => void
+}
 
-const CustomInput: React.FC<CustomInputProps> = ({value, onClick}) => (
-    <input
-        className={`${s.customDateInput}`}
-        value={value}
-        onClick={onClick}
-        readOnly
-    />
-);
+type InputProps = {
+  value: string
+  onClick: () => void
+  error?: boolean
+}
 
-export const UIDatePicker: React.FC<PropsType> = ({onChange, isRange}) => {
-    const [dateRange, setDateRange] = useState<[Date | null, Date | null]>([null, null]);
-    const [startDate, endDate] = dateRange;
+const CustomInput = ({ value, onClick, error }: InputProps) => {
+  const inputClassname = `${s.customDateInput} ${error ? s.error : ''}`
+  return <input className={inputClassname} value={value} onClick={onClick} readOnly />
+}
 
-    const formatDate = (date: Date | null): string => {
-        return date ? format(date, 'dd/MM/yyyy') : '';
-    };
+export const UIDatePicker = ({ onChange, isRange }: Props) => {
+  const [dateRange, setDateRange] = useState<[Date | null, Date | null]>([null, null])
+  const [startDate, endDate] = dateRange
 
-    return (
-        <DatePicker
-            selectsRange={isRange}
-            startDate={startDate}
-            dateFormat="dd/MM/yyyy"
-            endDate={endDate}
-            calendarStartDay={1}
-            selected={isRange ? null : startDate}
-            onChange={(update) => {
-                if (!Array.isArray(update)) {
-                    setDateRange([update, update]);
-                    if (onChange) {
-                        onChange(formatDate(update));
-                    }
-                } else {
-                    setDateRange(update);
-                    if (onChange) {
-                        onChange(`${formatDate(update[0])} - ${formatDate(update[1])}`);
-                    }
-                }
-            }}
-            isClearable={true}
-            customInput={
-            <CustomInput
-                onClick={() => {}}
-                value={isRange
-                    ? `${formatDate(startDate)} - ${formatDate(endDate)}`
-                    : `${formatDate(startDate)}`}
-            />}
+  const formatDate = (date: Date | null): string => {
+    return date ? format(date, 'dd/MM/yyyy') : ''
+  }
+
+  return (
+    <DatePicker
+      selectsRange={isRange}
+      startDate={startDate}
+      dateFormat="dd/MM/yyyy"
+      className="red-border"
+      endDate={endDate}
+      calendarStartDay={1}
+      selected={isRange ? null : startDate}
+      onChange={update => {
+        if (!Array.isArray(update)) {
+          setDateRange([update, update])
+          if (onChange) {
+            onChange(formatDate(update))
+          }
+        } else {
+          setDateRange(update)
+          if (onChange) {
+            onChange(`${formatDate(update[0])} - ${formatDate(update[1])}`)
+          }
+        }
+      }}
+      customInput={
+        <CustomInput
+          error={true}
+          onClick={() => {}}
+          value={
+            isRange
+              ? `${formatDate(startDate)} - ${formatDate(endDate)}`
+              : `${formatDate(startDate)}`
+          }
         />
-    );
-};
+      }
+      showIcon={true}
+      icon={<Icons.Calendar width={20} height={20} theme={'dark'} />}
+    />
+  )
+}
