18907469704c14deb29cdd55838bb020bcc607f9
stasikrus <stas-boss@mail.ru>
2024-01-31 15:19:05 +0300

    UC-57 feat: add HeadMeta component, refactor PolicySwitcher and pages with rules

--- src/pages/auth/terms-of-service/index.tsx
+++ src/pages/auth/terms-of-service/index.tsx
@@ -1,8 +1,14 @@
 import { PolicySwitcher } from '@/src/shared/components/PolicySwitcher/PolicySwitcher'
 import { getLayout } from '@/src/widgets/Layout/Layout'
+import { HeadMeta } from '@/src/widgets/HeadMeta/HeadMeta'
 
 const TermsOfService = () => {
-  return <PolicySwitcher privacyPolicy={false} />
+  return (
+    <>
+      <PolicySwitcher variant="terms" />
+      <HeadMeta title="Terms of Service" />
+    </>
+  )
 }
 
 TermsOfService.getLayout = getLayout
