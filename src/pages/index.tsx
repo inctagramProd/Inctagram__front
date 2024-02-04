66e36fb89fa0c1b3a2e4b958ad97e88ec270bba6
Alexander Ivantsov <alexander.ivantsov.hw@gmail.com>
2024-02-04 11:01:21 +0300

    test: test auth components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className={`flex min-h-screen flex-col items-center justify-between p-24`}>
       <GitAuth name="Git" />
-      {/* <GoogleAuth name="Google" /> */}
+      <GoogleAuth name="Google" />
       <div
         style={{
           display: 'flex',
