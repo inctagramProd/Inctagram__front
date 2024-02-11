8a6ca88c858a8de112a06bb6491e349e9205284a
PavelH <nesoglasnyj.pavel@gmail.com>
2024-02-11 12:13:22 +0300

    fix: change value select, type rootReducer.

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,13 +1,13 @@
 import { baseApi } from '@/src/shared/api/baseApi'
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/query/react'
 import { signInReducer } from '@/src/features/auth/signIn/model/signInSlice'
 import { loadState, saveState } from '@/src/shared/lib/localstorage'
 
-const rootReducer: any = {
+const rootReducer = combineReducers({
   [baseApi.reducerPath]: baseApi.reducer,
   signIn: signInReducer,
-}
+})
 export const store = configureStore({
   reducer: rootReducer,
   middleware: getDefaultMiddleware => getDefaultMiddleware().concat(baseApi.middleware),
