f70bb09bc6e8bf6d95c5c09148a8cdfac027895a
Alexander Ivantsov <alexander.ivantsov.hw@gmail.com>
2024-02-13 10:21:28 +0300

    deploy: stage project deploy

--- src/shared/api/ThirdPartyApi.ts
+++ src/shared/api/ThirdPartyApi.ts
@@ -4,10 +4,10 @@ export class api {
   static clientUrl =
     /* `http://localhost:3000/auth/sign-in/` */ 'https://inctagram-front.vercel.app/auth/sign-in/'
 
-  //GITHUB CONFIGS
+  //GITHUB CONFIGURATION
   static clientGitId: string = 'a4a56be1a1716484d5ea'
   static gitPath: string = `https://github.com/login/oauth/authorize?client_id=${this.clientGitId}&scope=read:user,user:email`
-  //GOOGLE CONFIGS
+  //GOOGLE CONFIGURATION
   static clientGoogleId: string =
     '621596965505-n57cqu1ifnmd34rqls715cao17t8r8n5.apps.googleusercontent.com'
   static googlePath = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${this.clientGoogleId}&redirect_uri=${this.clientUrl}&scope=email profile`
