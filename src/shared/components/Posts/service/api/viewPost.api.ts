0854141125e0f8dda368834aac10d1fc50a1b4b4
Alexander Ivantsov <alexander.ivantsov.hw@gmail.com>
2024-03-16 15:11:07 +0300

    testing

--- src/shared/components/Posts/service/api/viewPost.api.ts
+++ src/shared/components/Posts/service/api/viewPost.api.ts
@@ -1,28 +1,27 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { api } from '@/src/shared/api/ThirdPartyApi'
 
-const accessToken = (): string | null => {
-  const dataString = localStorage.getItem('Google Data')
-  if (dataString) {
-    console.log(dataString)
-    const data = JSON.parse(dataString)
-    if (data && data.accessToken && typeof data.accessToken === 'string') {
-      return data.accessToken
-    }
-  }
-  return null
-}
 
 export const viewPostsApi = createApi({
   reducerPath: 'posts',
-  baseQuery: fetchBaseQuery({ baseUrl: api.serverURL }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: api.serverURL,
+    prepareHeaders: (headers, { getState }) => {
+      const dataString = localStorage.getItem('Google Data')
+      if (dataString) {
+        const data = JSON.parse(dataString)
+        headers.set('Authorization', `Bearer ${data.accessToken}`)
+      }
+      return headers
+    },
+  }),
   endpoints: builder => ({
     viewPosts: builder.query({
       query: page => ({
         url: `/user-posts/${page}`,
-        headers: {
+        /*         headers: {
           Authorization: `Bearer ${accessToken()}`,
-        },
+        }, */
       }),
     }),
   }),
