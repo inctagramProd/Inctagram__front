229791cf1da1a4796c44055a5a8fffff8b7a037b
Alexander Ivantsov <alexander.ivantsov.hw@gmail.com>
2024-03-17 18:28:59 +0300

    test: test infinitive scrolling

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,17 +1,36 @@
 import Posts from '@/src/shared/components/Posts/Posts'
 import { useViewPostsQuery } from '@/src/shared/components/Posts/service/api/viewPost.api'
-import { LoaderSpin, Typography } from '@/src/shared/ui'
+import { Button, LoaderSpin, Typography } from '@/src/shared/ui'
 import { getLayoutWithSidebar } from '@/src/widgets/Layout/LayoutWithSidebar'
+import { useEffect, useState } from 'react'
 
 const ProfilePage = () => {
-  const { data, isLoading, isError, isFetching } = useViewPostsQuery(1)
+  const [page, setPage] = useState<number>(1)
+  const { data, isLoading, isError, isFetching } = useViewPostsQuery(page)
+  const posts = data ? data : []
+  useEffect(() => {
+    const onScroll = () => {
+      const scrolledToBottom = window.innerHeight + window.scrollY >= document.body.offsetHeight
+      if (scrolledToBottom && !isFetching) {
+        console.log('Fetching more data...')
+        setPage(page + 1)
+      }
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return function () {
+      document.removeEventListener('scroll', onScroll)
+    }
+  }, [page, isFetching])
+
   return (
     <div className="flex items-start justify-center w-full min-h-screen h-auto gap-[20px]">
       <div className="w-[46%] h-auto mt-[50px]">
         {isLoading ? (
           <LoaderSpin />
         ) : (
-          data.map((el: any, id: any) => {
+          posts.map((el: any, id: any) => {
             return (
               <Posts
                 name={'Alex'}
