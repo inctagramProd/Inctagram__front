1e7265d8e7a6781e28fb2bd838f0622a1848de14
Alexander Ivantsov <alexander.ivantsov.hw@gmail.com>
2024-03-23 17:30:03 +0300

    feat: add some style to ViewProfile

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -23,14 +23,129 @@ const ProfilePage = () => {
       document.removeEventListener('scroll', onScroll)
     }
   }, [page, isFetching])
-
+  const post = [
+    {
+      comments: [
+        {
+          name: 'Aex',
+          comment: ' Comment N1',
+          data: '12/03/2024',
+          avatar: 'https://www.pngall.com/wp-content/uploads/12/Male-Face-PNG-File.png',
+        },
+        {
+          name: 'Aex',
+          comment: ' Comment N2',
+          data: '12/03/2024',
+          avatar: 'https://imgpng.ru/d/face_PNG5658.png',
+        },
+        {
+          name: 'Aex',
+          comment: ' Comment N3',
+          data: '12/03/2024',
+          avatar: 'https://imgpng.ru/d/face_PNG5658.png',
+        },
+      ],
+      likes: [
+        {
+          name: 'Valera',
+          like: true,
+          avatar: 'https://www.pngall.com/wp-content/uploads/12/Male-Face-PNG-File.png',
+        },
+        {
+          name: 'Alex',
+          like: true,
+          avatar: 'https://imgpng.ru/d/face_PNG5658.png',
+        },
+        {
+          name: 'Luka',
+          like: true,
+          avatar: 'https://c3.klipartz.com/pngpicture/992/674/sticker-png-kanye-west.png',
+        },
+        {
+          name: 'David',
+          like: true,
+          avatar: 'https://www.pngall.com/wp-content/uploads/12/Male-Face-PNG-File.png',
+        },
+      ],
+      postDescription: 'It is first post',
+      postImages: [
+        {
+          imageUrl:
+            'https://img.goodfon.com/original/360x640/b/5d/sport-snoubord-sneg-spusk-gory-pryzhok-nebo-snoubording-1.jpg',
+        },
+        {
+          imageUrl: 'https://abali.ru/wp-content/uploads/2013/12/uporotij_lis.png',
+        },
+      ],
+      name: 'Alex',
+    },
+    {
+      comments: [],
+      likes: [],
+      postDescription: 'It is second post',
+      postImages: [
+        {
+          imageUrl: 'https://abali.ru/wp-content/uploads/2013/12/uporotij_lis.png',
+        },
+        {
+          imageUrl:
+            'https://img.goodfon.com/original/360x640/b/5d/sport-snoubord-sneg-spusk-gory-pryzhok-nebo-snoubording-1.jpg',
+        },
+      ],
+      name: 'Valera',
+    },
+    {
+      comments: [],
+      likes: [],
+      postDescription: null,
+      postImages: [
+        {
+          imageUrl:
+            'https://img.goodfon.com/original/360x640/b/5d/sport-snoubord-sneg-spusk-gory-pryzhok-nebo-snoubording-1.jpg',
+        },
+        {
+          imageUrl:
+            'https://img.goodfon.com/original/360x640/b/5d/sport-snoubord-sneg-spusk-gory-pryzhok-nebo-snoubording-1.jpg',
+        },
+      ],
+      name: 'Sveta',
+    },
+    {
+      comments: [],
+      likes: [],
+      postDescription:
+        'It is fouth post.saddddd ddd ddddd dddddddd ddddd ddd ddd ddddddd dddd dddddd ddddddddd dddddd dsssssss ssssssss sss ddd ddddddd  ssssss ddd ddddddd   ddd ddddd dddddddd ddddd ddd ddd ddddddd dddd dddddd ddddddddd dddddd dsssssss ssssssss sss ddd ddddddd  ssssss ddd ddddddd  ddd ddddddd  sss sssss ddd ddddddd  ssssss sss ddd ddddddd s sssss sssss sssss sssss ss  ddd ddddddd sssss sssssssss',
+      postImages: [
+        {
+          imageUrl:
+            'https://img.goodfon.com/original/360x640/b/5d/sport-snoubord-sneg-spusk-gory-pryzhok-nebo-snoubording-1.jpg',
+        },
+        {
+          imageUrl: 'https://abali.ru/wp-content/uploads/2013/12/uporotij_lis.png',
+        },
+      ],
+      name: 'Lexa',
+    },
+  ]
   return (
     <div className="flex items-start justify-center w-full min-h-screen h-auto gap-[20px]">
-      <div className="w-[46%] h-auto mt-[50px]">
-        {isLoading ? (
+      <div className="flex flex-col w-[46%] h-auto mt-[50px] gap-20">
+        {post.map((el: any, id: any) => {
+          return (
+            <Posts
+              name={'Alex'}
+              img={el.postImages}
+              postDescreption={el.postDescription}
+              likes={el.likes}
+              comments={el.comments}
+              key={id}
+            />
+          )
+        })}
+        {/* {isLoading ? (
           <LoaderSpin />
         ) : (
-          posts.map((el: any, id: any) => {
+          post.map((el: any, id: any) => {
             return (
               <Posts
                 name={'Alex'}
@@ -42,7 +157,7 @@ const ProfilePage = () => {
               />
             )
           })
-        )}
+        )} */}
       </div>
     </div>
   )
--- src/shared/components/Posts/Posts.tsx
+++ src/shared/components/Posts/Posts.tsx
@@ -1,28 +1,48 @@
 import { useEffect, useState } from 'react'
 import SliderArrow from './service/ui/sliderArrow/sliderArrow'
 
-import { Icon, Typography } from '../../ui'
+import { Icon, TextArea, Typography } from '../../ui'
+import { Button } from '@/src/shared/ui'
 
 type imgType = {
   imageId: number
   imageUrl: string
 }
+type likeType = {
+  name: string
+  like: boolean
+  avatar: string
+}
+type comment = {
+  name: string
+  comment: string
+  data: string
+}
 export type PostsProps = {
+  comments: any
   name: string
   img: imgType[]
   postDescreption: string | null
-  like: number | null
-  comments: string[] | null
+  likes: likeType[]
 }
-const Posts = ({ name, img, postDescreption, like, comments }: PostsProps) => {
+const Posts = ({ name, img, postDescreption, likes, comments }: PostsProps) => {
+  console.log(comments)
   const [imgItem, setImgItem] = useState<number>(0)
   const [visibleDescription, setVisibleDescription] = useState(false)
-
-  useEffect(() => {}, [imgItem])
+  const [textArea, setTextArea] = useState<boolean>(false)
+  useEffect(() => {}, [imgItem, visibleDescription])
+  const openDescription = () => {
+    console.log(`openDescription ${visibleDescription}`)
+    setVisibleDescription(!visibleDescription)
+  }
+  const openTextArea = () => {
+    setTextArea(!textArea)
+  }
   const Circle = []
   for (let i = 1; i <= img.length; i++) {
     Circle.push(i)
   }
+
   const CircleBlock = Circle.map((e, i, arr) => {
     return (
       <div
@@ -34,8 +54,8 @@ const Posts = ({ name, img, postDescreption, like, comments }: PostsProps) => {
     )
   })
   return (
-    <div className="flex flex-col gap-[5px]">
-      <div className="flex justify-between border border-solid border-indigo-500 w-full h-[30px] ">
+    <div className="flex flex-col">
+      <div className="flex justify-between w-full h-[30px] ">
         <div className="flex gap-1 items-center justify-center">
           <div className="rounded-full bg-dark-100 w-[30px] h-[30px]" />
           <Typography variant="bold_16">{name}</Typography>
@@ -47,11 +67,15 @@ const Posts = ({ name, img, postDescreption, like, comments }: PostsProps) => {
           />
         </div>
       </div>
-      <div className="border border-solid border-indigo-500 w-full h-auto">
+      <div className="w-full h-auto">
         <div className="d-flex align-center justify-center">
           <div className={'relative inline-block h-[400px] flex '}>
             <SliderArrow img={img} setImgItem={setImgItem} imgItem={imgItem} direction={'left'} />
-            <img src={img[imgItem].imageUrl} alt={'img'} className="w-full h-auto" />
+            <img
+              src={img[imgItem].imageUrl}
+              alt={'img'}
+              className="object-contain w-full h-full bg-dark-100/25"
+            />
             <SliderArrow img={img} setImgItem={setImgItem} imgItem={imgItem} direction={'right'} />
             <div className="absolute bottom-[10px] w-full flex  justify-center">
               <div className="w-auto bg-dark-100/50 flex flex-column justify-center gap-3 p-2">
@@ -61,7 +85,7 @@ const Posts = ({ name, img, postDescreption, like, comments }: PostsProps) => {
           </div>
         </div>
       </div>
-      <div className="border border-solid border-indigo-500 w-full h-[30px] flex  justify-between w-full">
+      <div className="w-full h-[30px] flex  justify-between w-full">
         <div className="flex gap-3">
           <Icon
             iconName="Heart"
@@ -83,27 +107,33 @@ const Posts = ({ name, img, postDescreption, like, comments }: PostsProps) => {
       </div>
 
       {postDescreption ? (
-        <div className="border border-solid border-indigo-500 w-full min-h-[60px] h-auto flex gap-1">
+        <div className="w-full min-h-[60px] h-auto flex gap-1">
           <div className="rounded-full bg-dark-100 w-[30px] h-[30px]" />
 
           <div className="w-[90%] text-justify">
-            <Typography variant="bold_16" className="w-auto">
-              {name}
-            </Typography>
             <div>
               <div>
-                <Typography
-                  variant="regular_14"
-                  className={`h-${visibleDescription ? 'auto' : '20'} overflow-hidden`}
+                <div
+                  className={`${
+                    visibleDescription === true ? 'h-auto' : 'm-h-10'
+                  } overflow-hidden text-justify h-20`}
                 >
-                  {postDescreption}{' '}
-                </Typography>
+                  {' '}
+                  <Typography
+                    variant="regular_14"
+                    className={`${
+                      visibleDescription === true ? 'h-auto' : 'm-h-10'
+                    } overflow-hidden text-justify h-20`}
+                  >
+                    <span className="inline-block font-bold">{name}</span> {postDescreption}{' '}
+                  </Typography>
+                </div>
                 <Typography
                   variant="small"
                   className={
                     'text-dark-100 cursor-pointer italic hover:text-dark-300 transition duration-300 ease-in-out'
                   }
-                  onClick={() => setVisibleDescription(!visibleDescription)}
+                  onClick={openDescription}
                 >
                   {visibleDescription ? 'свернуть' : 'еще'}
                 </Typography>
@@ -112,9 +142,63 @@ const Posts = ({ name, img, postDescreption, like, comments }: PostsProps) => {
           </div>
         </div>
       ) : null}
-      <div className="border border-solid border-indigo-500 w-full h-[30px]">Likes:{like}</div>
-      <div className="border border-solid border-indigo-500 w-full h-[30px]">
-        Comments:{comments}
+      {likes?.length > 0 ? (
+        <div className="flex flex-row w-full h-[30px] ">
+          {likes
+            ?.filter((e, i) => (i <= 2 ? e : null))
+            .map((el, index) => {
+              return (
+                <div className="d-flex align-start justify-center" key={index}>
+                  <img
+                    src={el.avatar}
+                    className="w-5 h-5"
+                    onClick={() => {
+                      console.log(`likes ${el.name}`)
+                    }}
+                  />
+                </div>
+              )
+            })}
+          {`   ${likes?.length}`} <span className="inline-block font-bold">"Like"</span>
+        </div>
+      ) : null}
+      <div className=" w-full m-h-[30px] h-auto">
+        <span className="font-bold cursor-pointer" onClick={() => console.log(1)}>
+          {' '}
+          {comments?.length > 0
+            ? `View all comments (${comments?.length}) `
+            : 'There are no comments'}
+        </span>
+        {comments.map((el: any, i: any) => {
+          return (
+            <div key={i} className="flex gap-1">
+              <div className="flex justify-start align -center  h-100 w-auto ">
+                <img src={el.avatar} className={`h-5 w-5`} />
+              </div>
+              <div>
+                <div>
+                  <span className="inline-block font-bold">{el.name}</span>
+                  {`${el.comment}`}
+                </div>
+                <Typography
+                  variant="small"
+                  className={
+                    'text-dark-100 cursor-pointer italic hover:text-dark-300 transition duration-300 ease-in-out'
+                  }
+                >
+                  {el.data}
+                </Typography>
+              </div>
+            </div>
+          )
+        })}
+        <div className={'flex justify-between border-b-2 border-gray'}>
+          <span className="text-dark-100 cursor-pointer italic" onClick={openTextArea}>
+            Add comment...
+          </span>{' '}
+          {textArea ? <TextArea /> : null}
+          <Button label="Publish" style={'text'} />
+        </div>
       </div>
     </div>
   )
