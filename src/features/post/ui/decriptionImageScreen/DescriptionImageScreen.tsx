e84af6d74ab99791af681584c03646587192d73c
PavelH <nesoglasnyj.pavel@gmail.com>
2024-04-14 23:07:35 +0300

    UC-126 feat: add style DescriptionImageScreen

---
+++ src/features/post/ui/decriptionImageScreen/DescriptionImageScreen.tsx
@@ -0,0 +1,106 @@
+import React, { useState } from 'react'
+import { ImageURL } from '@/src/entities/post/model/types/postSliceTypes'
+import Image from 'next/image'
+import { Button, TextArea, Typography } from '@/src/shared/ui'
+import { useTranslate } from '@/src/app/hooks/useTranslate'
+import { useCreatePostMutation } from '@/src/features/post/service/createPostApi'
+import { getModifiedImage } from '@/src/shared/helpers/canvasUtils'
+import { SlickSlider } from '@/src/shared/ui/Slider/Slider'
+
+const MAX_NUMBER_OF_CHARACTERS = 500
+
+type Props = {
+  imagesWithFilters: ImageURL[]
+  closeModal: () => void
+}
+
+export const DescriptionImageScreen = ({ imagesWithFilters, closeModal }: Props) => {
+  const [createPost, { isLoading, isSuccess }] = useCreatePostMutation()
+  const [textPostDescription, setTextPostDescription] = useState<string>('')
+  const { locale } = useTranslate()
+
+  const handleLoadImages = (imagesArray: ImageURL[]) => {
+    const formData = new FormData()
+    const imagesFiles = imagesArray.map(el =>
+      getModifiedImage({
+        imageSrc: el.imageURL,
+        mode: 'blob',
+      })
+    )
+    return Promise.all(imagesFiles).then(res => {
+      res.forEach(el => {
+        formData.append('files', el as Blob)
+      })
+      return formData
+    })
+  }
+
+  const createPostHandler = () => {
+    handleLoadImages(imagesWithFilters).then(res => {
+      console.log({ files: res, description: textPostDescription })
+      createPost({ files: res, description: textPostDescription })
+      if(isSuccess){
+        closeModal()
+      }
+    })
+  }
+
+  return (
+    <div className="flex gap-6">
+      <div className="w-1/2">
+        <SlickSlider isShowNavigation={imagesWithFilters.length !== 1}>
+          {imagesWithFilters.map((img, i) => (
+            <Image
+              key={img.imageURL + i}
+              src={img.imageURL}
+              alt={img.imageURL}
+              width={100}
+              height={100}
+              className="object-cover w-full h-[506px]"
+            />
+          ))}
+        </SlickSlider>
+      </div>
+      <div className="w-1/2 p-6 flex flex-col">
+        <div className="">
+          <Typography variant="regular_14" className="text-light-900">
+            {locale.profile.addNewPost.addDescription}
+          </Typography>
+          <TextArea
+            errorMessage={`${
+              textPostDescription.length > MAX_NUMBER_OF_CHARACTERS
+                ? locale.profile.descriptionError.error
+                : ''
+            }`}
+            className="h-[120px] resize-none overflow-y-auto scrollbar-thin scrollbar-track-dark-300 scrollbar-thumb-primary-700"
+            value={textPostDescription}
+            onChange={e => {
+              setTextPostDescription(e.target.value)
+            }}
+          />
+          <Typography
+            variant="small"
+            className={`${
+              textPostDescription.length > MAX_NUMBER_OF_CHARACTERS
+                ? 'text-danger-500'
+                : 'text-light-900'
+            } text-end`}
+          >
+            {textPostDescription.length}/{MAX_NUMBER_OF_CHARACTERS}
+          </Typography>
+        </div>
+        <div className="flex-1 flex items-end justify-end">
+          <Button
+            disable={textPostDescription.length > MAX_NUMBER_OF_CHARACTERS}
+            variant="regular_14"
+            style="primary"
+            label={locale.profile.addNewPost.publication}
+            onClick={() => {
+              createPostHandler()
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -68,6 +68,7 @@ const config: Config = {
     plugins: [
         require('tailwindcss'),
         require('autoprefixer'),
+        require('tailwind-scrollbar'),
         // Add other Tailwind CSS plugins if needed
     ],
 }
