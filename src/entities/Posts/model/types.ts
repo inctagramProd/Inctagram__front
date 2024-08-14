7e2c4441a291c8e5d1bb093eb8abb799596d4e0e
Richard Leshik <richardleshik@gmail.com>
2024-08-14 20:41:50 +0300

    UC-12 refactor: remove props types from types.ts

--- src/entities/Posts/model/types.ts
+++ src/entities/Posts/model/types.ts
@@ -1,10 +1,8 @@
-import { SetStateAction } from 'react'
-
-type Img = {
+export type Img = {
   imageId: number
   imageUrl: string
 }
-type Like = {
+export type Like = {
   name: string
   like: boolean
   avatar: string
@@ -14,35 +12,4 @@ export type Comment = {
   comment: string
   date: string
   avatar: string
-}
-
-export type CircleBlocksProps = {
-  imagesLen: number
-  imgNumber: number
-  setImgNumber: (imgNumber: number) => void
-}
-
-export type PostProps = {
-  name: string
-  avatarUrl: string
-  postDescription: string | null
-  images: Img[]
-  likes: Like[]
-  comments: Comment[]
-}
-export type SliderArrowProps = {
-  imgItem: number
-  direction: 'left' | 'right'
-  images: Img[]
-  setImgItem: (value: SetStateAction<number>) => void
-}
-export type PostIconButtonProps = {
-  iconName: string
-  logMessage: string
-}
-export type AvatarImageLinkProps = {
-  size?: 'small' | 'regular'
-  src?: string
-  className?: string
-  onClickImage?: () => void
 }
