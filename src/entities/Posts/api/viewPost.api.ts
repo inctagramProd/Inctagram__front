05661cefddfcda0af1749b888537b235db6c0322
Richard Leshik <richardleshik@gmail.com>
2024-08-17 11:44:57 +0300

    UC-12 refactor: rebase getAccessToken function to shared/lib

--- src/entities/Posts/api/viewPost.api.ts
+++ src/entities/Posts/api/viewPost.api.ts
@@ -1,16 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import { api } from '@/src/shared/api/ThirdPartyApi'
-
-const getAccessToken = (): string => {
-  const tokenData = JSON.parse(
-    localStorage.getItem('Google Data') ||
-      localStorage.getItem('Git Data') ||
-      localStorage.getItem('accessToken') ||
-      ''
-  )
-
-  return tokenData?.accessToken || ''
-}
+import { api, getAccessToken } from '@/src/shared'
 
 export const viewPostsApi = createApi({
   reducerPath: 'posts',
--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,1 +1,4 @@
+export { api } from './api/ThirdPartyApi'
+
+export { getAccessToken } from './lib/getAccessToken'
 export { isNotEmpty } from './utils/isNotEmpty'
