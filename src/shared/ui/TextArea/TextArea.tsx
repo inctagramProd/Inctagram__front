0583c4ea5e3d57b319f96efacf65f6d22d2b6777
Richard Leshik <richardleshik@gmail.com>
2024-08-17 14:58:25 +0300

    UC-12 feat: update TextArea.tsx for comments

--- src/shared/ui/TextArea/TextArea.tsx
+++ src/shared/ui/TextArea/TextArea.tsx
@@ -1,26 +1,71 @@
-import { ComponentPropsWithoutRef } from 'react'
+import { ComponentPropsWithoutRef, useEffect, useRef, useState } from 'react'
+import type { PageText } from '@/src/entities/Posts/model/types'
 
 type Props = {
   errorMessage?: string
+  type?: 'default' | 'comment'
+  pageText: PageText
+  className?: string
 } & ComponentPropsWithoutRef<'textarea'>
 
-export const TextArea = ({ errorMessage, ...restProps }: Props) => {
+export const TextArea = ({
+  errorMessage,
+  type = 'default',
+  pageText,
+  className,
+  ...restProps
+}: Props) => {
+  const [hasContent, setHasContent] = useState(false)
+  const textareaRef = useRef<HTMLTextAreaElement>(null)
+
+  useEffect(() => {
+    if (textareaRef.current) {
+      setHasContent(textareaRef.current.value.trim() !== '')
+    }
+  }, [textareaRef])
+
+  const handleInputChange = () => {
+    setHasContent(textareaRef.current?.value.trim() !== '')
+
+    if (textareaRef.current !== null) {
+      textareaRef.current.style.height = textareaRef.current.scrollHeight + 'px'
+    }
+  }
+
   return (
-    <div>
-      <textarea
-        className={` 
-        w-full outline-none placeholder-light-900 border 
-        rounded-sm py-1.5 px-3 bg-dark-500
-        disabled:placeholder-dark-100 disabled:text-dark-100
-        focus:ring-1 
-        ${
-          errorMessage
-            ? 'border-danger-500 focus:border-danger-500 focus:ring-danger-500'
-            : 'border-dark-100 focus:border-primary-700 focus:ring-primary-700'
-        }`}
-        {...restProps}
-      />
-      {errorMessage && <span className="text-danger-500">{errorMessage}</span>}
-    </div>
+    <>
+      {type === 'comment' ? (
+        <div className="relative">
+          <textarea
+            id="com"
+            ref={textareaRef}
+            className={`w-full outline-none placeholder-light-900 rounded-sm py-1.5 px-3 h-auto bg-dark-700 border-light-900 focus:border-b focus:outline-none focus:border-primary-300 transition-colors peer border-b overflow-hidden resize-none ${className}`}
+            {...restProps}
+            rows={1}
+            onInput={handleInputChange}
+          />
+          <label
+            htmlFor="com"
+            className={`absolute left-0 top-1 text-light-900 text-s cursor-text peer-focus:text-xs peer-focus:-top-4 peer-focus:text-primary-300 transition-all ${
+              hasContent ? 'text-xs -top-4 text-primary-300' : ''
+            } ${className}`}
+          >
+            {pageText.addComment}
+          </label>
+        </div>
+      ) : (
+        <>
+          <textarea
+            className={`w-full outline-none placeholder-light-900 border rounded-sm py-1.5 px-3 bg-dark-500 disabled:placeholder-dark-100 disabled:text-dark-100 focus:border-b ${
+              errorMessage
+                ? 'border-danger-500 focus:border-danger-500 focus:ring-danger-500'
+                : 'border-dark-100 focus:border-primary-700 focus:ring-primary-700'
+            }`}
+            {...restProps}
+          />
+          {errorMessage && <span className="text-danger-500">{errorMessage}</span>}
+        </>
+      )}
+    </>
   )
 }
